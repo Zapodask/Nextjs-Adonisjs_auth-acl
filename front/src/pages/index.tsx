61bdf1b451bcb20c57326c6a930039316755cf55
Zapodask <joaopedroemerim@gmail.com>
2021-04-01 00:25:03 -0300

    .

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import Layout from '@/components/layout'
 
-import { Form } from '@unform/web'
-import { Input } from '@/components/input'
-
 const Index: React.FC = () => {
   return (
     <Layout title='Home'>
-      <Form onSubmit={() => console.log('a')}>
-        <Input name='name' label='Name' />
-      </Form>
+      Home
     </Layout>
   )
 }
